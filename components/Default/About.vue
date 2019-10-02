f06c3997c2cd5e798d929bff2a904145946b573f
pkahfi <pkahfi@dwebsite.net>
2019-10-02 13:26:50 +0700

    update resume layout - add education component, store - call education component from experiences - update resume.json get data from kickresume examples data - fix some layout

--- components/Default/About.vue
+++ components/Default/About.vue
@@ -1,12 +1,12 @@
 <template>
   <section class="flex pb-2 mt-5 border-separate">
     <div class="w-2/4 mr-3">
-      <h1 class="text-1xl font-serif mb-0 pb-0 text-gray-600 font-bold uppercase mb-3">About Me</h1>
+      <h1 class="text-1xl font-serif mb-0 pb-0 font-bold uppercase mb-3">About Me</h1>
       <h3 class="text-1xl text-gray-600 mb-3 pt-0">{{ info.summary }}</h3>
     </div>
 
     <div class="w-1/4 object-right tracking-wider">
-      <h1 class="text-1xl font-serif mb-0 pb-0 text-gray-600 font-bold uppercase mb-3">Contact</h1>
+      <h1 class="text-1xl font-serif mb-0 pb-0 font-bold uppercase mb-3">Contact</h1>
       <h3 class="text-sm text-gray-600">
         <fa-layers full-width class="fa-1x">
           <fa :icon="['fas', 'phone']" class="mr-2"  />

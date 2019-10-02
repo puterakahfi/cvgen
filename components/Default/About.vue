a2f29836b2fdbf18620031eeb9e032441df524bf
puterakahfi <puterakahfi@gmail.com>
2019-10-02 18:48:44 +0700

    tweak layout - make some adjustment to title size - update resume.json content - upgrade font awesome version

--- components/Default/About.vue
+++ components/Default/About.vue
@@ -1,28 +1,28 @@
 <template>
-  <section class="flex pb-2 mt-5 border-separate">
-    <div class="w-4/6 mr-3">
-      <h1 class="text-1xl font-serif mb-0 pb-0 font-bold uppercase mb-3">About Me</h1>
-      <h3 class="text-1xl text-gray-600 mb-3 pt-0">{{ info.summary }}</h3>
+  <section class="lg:flex sm:block  pb-2 mt-5 border-separate">
+    <div class="w-4/6  mr-3">
+      <h1 class="text-2xl font-serif mb-0 pb-0 font-bold  mb-3">About Me</h1>
+      <h3 class="text-md  mb-3 pt-0">{{ info.summary }}</h3>
     </div>
 
-    <div class="w-2/6 object-right tracking-wider">
-      <h1 class="text-1xl font-serif mb-0 pb-0 font-bold uppercase mb-3">Contact</h1>
-      <h3 class="text-sm text-gray-600">
+    <div class="w-2/6 md:w-6/6 object-right tracking-wider">
+      <h1 class="text-1xl font-serif mb-0 pb-0 font-bold  mb-4">Contact</h1>
+      <h3 class="text-sm">
         <fa-layers full-width class="fa-1x">
-          <fa :icon="['fas', 'phone']" class="mr-2"  />
+          <fa :icon="['fas', 'phone']"  size="sm" class="mr-2" />
         </fa-layers>
         {{ info.phone }}
       </h3>
-      <h3 class="text-sm text-gray-600 pt-3">
+      <h3 class="text-sm pt-3">
         <fa-layers full-width class="fa-1x">
-          <fa :icon="['fas', 'envelope']" class="mr-2"  />
+          <fa :icon="['fas', 'envelope']"  size="sm" class="mr-2" />
         </fa-layers>
         {{ info.email }}
       </h3>
 
-      <h3 class="text-sm text-gray-600 pt-3">
+      <h3 class="text-sm  pt-3">
         <fa-layers full-width class="fa-1x">
-          <fa :icon="['fas', 'home']" class="mr-2"  />
+          <fa :icon="['fas', 'home']" size="sm"  class="mr-2" />
         </fa-layers>
         {{ info.website }}
       </h3>
